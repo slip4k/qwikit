55778ed8f0f18cd19c4dda75428f0163aaec5ce8
Justin Shim <justinjaeshim@gmail.com>
2024-05-15 16:58:29 -0700

    Initial Commit

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { ThemeProvider } from '@/components/theme-provider';
+import { ModeToggle } from '@/components/mode-toggle';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'qwikit - Start your Next.js application qwik with qwikit',
+  description:
+    'Qwikit is a boilerplat for Next.js applications including Authentication, payments, and analytics.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          disableTransitionOnChange
+        >
+          {children}
+          <ModeToggle />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
