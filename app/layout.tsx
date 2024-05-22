897b8f1a3bccec570b295967b9bed1fe95ff7e69
Justin Shim <justinjaeshim@gmail.com>
2024-05-22 15:17:32 -0700

    Incorporate NavBar Styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
 import { ModeToggle } from '@/components/mode-toggle';
+import HomeNavBar from '@/components/home-navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
           defaultTheme="dark"
           disableTransitionOnChange
         >
+          <HomeNavBar />
           {children}
           <ModeToggle />
         </ThemeProvider>
--- components/home-navbar.tsx
+++ components/home-navbar.tsx
@@ -1,7 +1,18 @@
-export default function HomeNavBar () {
+import { Button } from './ui/button';
+
+export default function HomeNavBar() {
   return (
     <div>
-      Hi
+      <div className="flex justify-between  m-3 rounded-xl h-[75px] px-[4dvw] z-50 ">
+        <div className="flex items-center justify-center text-5xl font-bold">
+          js.
+        </div>
+
+        <div className="flex gap-x-5 items-center mr-2">
+          <Button>Log In</Button>
+          <Button variant="secondary">Sign Up</Button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
